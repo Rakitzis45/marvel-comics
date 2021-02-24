6914e540abf0b94a0e12171b566f9623dd19140d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-24 13:09:54 -0500

    Create CharacterContainer.js

---
+++ src/containers/CharacterContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Characters from './components/Characters'
+
+
+class CharacterContainer extends Component {
+    render(){
+        return (
+            <div><Characters/></div>
+        )
+    }
+}
+
+export default CharacterContainer
