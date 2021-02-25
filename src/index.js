ad3213206695b2053a4258a498d390433eb13da1
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-25 16:02:34 -0500

    import applyMiddleWare and thunk

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import App from './App';
 
 import reducer from './reducer/reducer'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+const store = createStore(reducer, applyMiddleware(thunk));
+// , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ReactDOM.render(
   <Provider store={store}>
     <App />
