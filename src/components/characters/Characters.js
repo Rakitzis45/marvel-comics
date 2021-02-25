49038628274ebb80c65044e6ab78c23297cbb211
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-25 15:59:14 -0500

    Update Characters.js

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,3 +1,18 @@
 import React, { Component } from 'react';
+import Character from './Character';
 
-class Characters extends Component {}
+class Characters extends Component {
+
+    render(){
+        return (
+            <>
+            <h1>Characters</h1>
+                <ol>
+                    {this.props.characters ? this.props.characters.map(character => <Character key={character.id} character={character} />) : "No Characters"}
+                </ol>
+            </>
+        )
+    }
+}
+
+export default Characters
