3042c2be038585aad7f388b9757c09f98a4a0b41
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-25 16:01:23 -0500

    remove apiCall

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
 import SearchInput from '../components/search/SearchInput'
 import { connect } from 'react-redux'
+import { fetchCharacters} from '../actions/fetchCharacters'
 
 
 class SearchContainer extends Component {
 
-    apiCall = (string) => {
-        let md5 = require('md5')
-        let date = new Date
-        let timeStamp = date.getTime()
-        let hash = md5(timeStamp + "69e7fca49effd63d49364bbe5fe0d748903ec689" + "7d4cc4c53258b0c0a55297421f2f233f")
-        
-        fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${string}&apikey=7d4cc4c53258b0c0a55297421f2f233f&hash=${hash}`)
-        .then(resp => resp.json())
-        .then(json => this.props.addSearch(json.data.results))
-      }
+    // componentDidMount(){
+    //     console.log(this.props)
+    //     // this.props.addSearchWithinDispatch()
+    // }
 
     render(){
         return (
             <div>
-                <SearchInput apiCall={this.apiCall} addSearch={this.props.addSearch}/>
+                <SearchInput fetchCharacters={this.props.fetchCharacters}/>
             </div>
         )
     }
 }
 
-const mapDistpatchToProps = dispatch => ({
-    addSearch: object => dispatch({type: "ADD_SEARCH", object})
-    
+const mapDispatchToProps = dispatch => ({
+    fetchCharacters: () => dispatch(fetchCharacters())
 })
 
-export default connect(null, mapDistpatchToProps)(SearchContainer)
+// function mapDistpatchToProps(dispatch){
+//     return {
+//         addSearchWithinDispatch: () => dispatch(fetchCharacters())
+//     }
+// }
+
+export default connect(null, mapDispatchToProps)(SearchContainer)
