5c48f203618f4ee9f7602181ba371e96b1dd69c0
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-26 17:41:37 -0500

    import redux-logger to use redux_devtools_extention

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 
-
-import reducer from './reducer/reducer'
+// import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
+import reducer from './reducer/reducer';
+import logger from 'redux-logger'
 
-const store = createStore(reducer, applyMiddleware(thunk));
-// , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk, logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
