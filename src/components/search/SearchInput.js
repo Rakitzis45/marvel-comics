98be941bd7f706bbb8a9daa271c0e3a9f3c27d53
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-27 13:19:24 -0500

    update state keys and naming of event listeners

--- src/components/search/SearchInput.js
+++ src/components/search/SearchInput.js
@@ -1,41 +1,66 @@
 import React, { Component } from 'react';
 import { withRouter} from 'react-router-dom';
 import { fetchCharacters } from '../../actions/fetchCharacters'
+import { fetchComics } from '../../actions/fetchComics'
 import {connect} from 'react-redux'
 
 class SearchInput extends Component {
 
     state = {
-        search_field: ""
+        search_character: "",
+        search_comic: ""
     }
 
-    onChange = event => {
+    characterChange = event => {
         this.setState({
-            search_field: event.target.value
+            search_character: event.target.value
         })
     }
 
-    onSubmit = (e) => {
+    comicChange = event => {
+        this.setState({
+            search_comic: event.target.value
+        })
+        
+    }
+
+    onSubmitCharacter = (e) => {
         e.preventDefault()
-        this.props.fetchCharacters(this.state.search_field)
+        this.props.fetchCharacters(this.state.search_character)
         this.props.history.push('/characters')
+    }
+
+    onSubmitComic = (e) => {
+        e.preventDefault()
+        this.props.fetchComics(this.state.search_comic)
+        this.props.history.push('/comics')
     }
 
     render(){
         return (
             <>
-                <form onSubmit={this.onSubmit}>
-                    <label>Search Characters</label>
-                    <input type="text" value={this.state.search_field} onChange={this.onChange}/>
+                <form onSubmit={this.onSubmitCharacter}>
+                    <label>Search Characters: </label>
+                    <input type="text" value={this.state.search_character} onChange={this.characterChange}/>
                     <input type="submit"/>
                 </form>
+                <br/>
+                <form onSubmit={this.onSubmitComic}>
+                    <label>Search Comics: </label>
+                    <input type="text" value={this.state.search_comic} onChange={this.comicChange}/>
+                    <input type="submit"/>
+                </form>
+                
             </>
         )
     }
 }
 
 function mapDispatchToProps(dispatch){
-    return {fetchCharacters: (string) => dispatch(fetchCharacters(string))}
+    return {
+        fetchCharacters: (string) => dispatch(fetchCharacters(string)),
+        fetchComics: (string) => dispatch(fetchComics(string))
+    }
 }
 export default withRouter(connect(null, mapDispatchToProps)(SearchInput))
 // export default connect(null, mapDisptachToProps)(SearchInput)
