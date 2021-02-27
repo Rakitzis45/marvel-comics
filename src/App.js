0948052a9e4987da6bc73770bf1358b2cc1017f0
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-27 13:17:31 -0500

    add routes and import ComicContainer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchContainer from './containers/SearchContainer'
 import CharacterContainer from './containers/CharacterContainer'
 import CharacterShow from './components/characters/CharacterShow'
 import ComicShow from './components/comics/ComicShow'
+import ComicContainer from './containers/ComicContainer'
 
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
@@ -17,7 +18,9 @@ class App extends Component {
             <Route exact path='/comics/:id' component={ComicShow} />
             <Route exact path='/characters/:id' component={CharacterShow} />
             <Route exact path='/search' component={SearchContainer} />
+            <Route exact path='/' component={SearchContainer} />
             <Route exact path='/characters' component={CharacterContainer}/>
+            <Route exact path='/comics' component={ComicContainer}/>
           </div>
           <a href="/search">Search again</a>
         </Router>
