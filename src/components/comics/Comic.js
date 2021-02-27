0f3fc814325f9c0a842d258ff5f29e84d1a7f19e
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-02-27 13:18:01 -0500

    Update Comic.js

---
+++ src/components/comics/Comic.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Comic extends Component {
+
+    render() {
+       return (
+            <> 
+                <li><a href={`/comics/${this.props.comic.id}`}>{this.props.comic.title}</a></li>
+            </>
+        )
+    }
+}
+
+export default Comic
