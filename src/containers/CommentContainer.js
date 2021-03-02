1df5ffec203b8a98dd74018a68aa50860713b816
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-01 20:17:47 -0500

    Update CommentContainer.js

--- src/containers/CommentContainer.js
+++ src/containers/CommentContainer.js
@@ -1,17 +1,51 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-// import Comments from '../components/comments/Comments'
+import Comments from '../components/comments/Comments'
 import CommentInput from '../components/comments/CommentInput'
+import { fetchComments } from '../actions/fetchComments'
 
-class CommentContainer extends Component {
+
+class CommentContainer extends PureComponent {
+
+    componentDidMount() {
+        this.props.fetchComments()
+    }
+
+    // shouldComponentUpdate(nextState){
+    //     debugger
+    //     if (this.props.comments !== nextState.comments){
+    //         debugger
+    //         return true
+    //     }
+    //     return false
+    // }
+
+    handleClick = (e) => {
+        this.props.fetchComments()
+    }
+
     render(){
         return ( 
             <div>
-                
+                <button onClick={this.handleClick}>Comments</button>
                 <CommentInput/>
+                <Comments comments={this.props.comments}/>
             </div>
         )
     }
 }
 
-export default CommentContainer
+const mapStateToProps = (state) => {
+    return {
+        comments: state.comments
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchComments: () => dispatch(fetchComments())
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps )(CommentContainer)
