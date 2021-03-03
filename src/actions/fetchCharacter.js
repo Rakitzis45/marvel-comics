f79de53861100c3246cea1db7ceeb48a24fabc6d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-03 12:30:47 -0500

    hide api_keys

--- src/actions/fetchCharacter.js
+++ src/actions/fetchCharacter.js
@@ -1,12 +1,13 @@
+
 export function fetchCharacter(string){
     return (dispatch) => {
         dispatch({ type: 'START_ADDING_CHARACTER_REQUEST'});
         let md5 = require('md5')
         let date = new Date()
         let timeStamp = date.getTime()
-        let hash = md5(`${timeStamp} + "69e7fca49effd63d49364bbe5fe0d748903ec689" + "7d4cc4c53258b0c0a55297421f2f233f"`)
+        let hash = md5(`${timeStamp} + ${process.env.REACT_APP_API_PRIVATE_KEY} + ${process.env.REACT_APP_PUBLIC_KEY}`)
     
-        fetch(`https://gateway.marvel.com:443/v1/public/characters/${string}?apikey=7d4cc4c53258b0c0a55297421f2f233f&hash=${hash}`)
+        fetch(`https://gateway.marvel.com:443/v1/public/characters/${string}?apikey=${process.env.REACT_APP_API_PUBLIC_KEY}&hash=${hash}`)
         .then(resp => resp.json())
         .then(character => dispatch({type: 'ADD_CHARACTER', character}))
        
