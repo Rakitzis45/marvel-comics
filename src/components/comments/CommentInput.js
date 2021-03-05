e0b7e11e36c7ea7c00666d7bc36077ca4e009c89
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-05 18:08:01 -0500

    Update CommentInput.js

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -12,23 +12,24 @@ class CommentInput extends PureComponent {
     }
 
     handleNameChange = (event) => {
-        this.setState({ 
+        this.setState({
             name: event.target.value
         })
     }
 
     handlePostChange = (event) => {
-        this.setState({ 
+        this.setState({
             post: event.target.value
         })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        
+
         this.props.addComment(this.state);
         submitComment(this.state);
-        this.setState({ ...this.state,
+        this.setState({
+            ...this.state,
             name: "",
             post: ""
         })
@@ -36,26 +37,34 @@ class CommentInput extends PureComponent {
 
 
 
-    render(){
-        return(
-            <>
-                <h3>Leave a Comment</h3><br/>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Name: </label>
-                    <input type='text' value={this.state.name} onChange={this.handleNameChange} /><br/>
-                    <label>Comment: </label>
-                    <input type='text_area' value={this.state.post} onChange={this.handlePostChange} /><br/>
-                    <input type='submit' />
+    render() {
+        return (
+            <div>
+                <h3>Leave a Comment</h3><br />
+                <form className="form-block" onSubmit={this.handleSubmit}>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <div className="form-group">
+                                <input className="form-input" placeholder="Your Name" type='text' value={this.state.name} onChange={this.handleNameChange} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xs-12">
+                        <div className="form-group">
+                            <textarea className="form-input" value={this.state.post} onChange={this.handlePostChange} required="" placeholder="Your Comment"></textarea>
+                        </div>
+                    </div>
+                    <input className="btn btn-primary center" type='submit' />
                 </form>
-            </>
+            </div>
         )
     }
 
 }
 
-function mapDispatchToProps(dispatch){
-    return{
-        addComment:(object) => dispatch(addComment(object))
+function mapDispatchToProps(dispatch) {
+    return {
+        addComment: (object) => dispatch(addComment(object))
     }
 }
 
