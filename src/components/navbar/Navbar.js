d2bb033a08c5f413cdd81724592d782876cef6cd
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-07 13:33:23 -0500

    Create Navbar.js

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+import  NavbarSearch from './NavbarSearch'
+
+export default class Navbar extends PureComponent {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href='/search'>Marvel Comics Search</a>
+                <NavbarSearch/>
+            </nav>
+        )
+    }
+}
