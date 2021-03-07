f00d72ad85ef5eeecf4eef2f269cf477440ac3b5
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-07 13:32:44 -0500

    add classnames for styling

--- src/components/comics/ComicCard.js
+++ src/components/comics/ComicCard.js
@@ -1,13 +1,16 @@
 import React, { PureComponent } from 'react'
 
 export default class ComicCard extends PureComponent {
-    
+
     displayCharacters = () => {
         let characters = this.props.comic.characters.items
         return (
-            <ol>
-                {characters.map((character, index) => <li key={index}><a href={`/characters/${character.resourceURI.split('/')[6]}`}>{character.name}</a></li>)}
-            </ol>
+            <span className='characterContainer'>
+                <ol>
+                    {characters.map((character, index) => <li key={index}><a href={`/characters/${character.resourceURI.split('/')[6]}`}>{character.name}</a></li>)}
+                </ol>
+            </span>
+
         )
     }
 
@@ -15,26 +18,24 @@ export default class ComicCard extends PureComponent {
         return (date.split('T')[0])
     }
 
-    
-    
-    render(){
+
+
+    render() {
         let comic = this.props.comic
         return (
-        <div>
-            
-            <div>
-                <h1>{comic.title}</h1>
-                <img className='comicShowImg' src={comic.thumbnail.path + ".jpg"} alt="comicCover"/>
-                <div className='comicDescription'>
-                    <strong>Description: </strong> {comic.description} <br/>
-                    <strong>Release Date: </strong> {this.displayDate(comic.dates[0].date)}<br/>
+            <div className="row align-items-center- my-5">
+                <div className="col-lg-7">
+                    <img className='img-fluid rounded mb-4 mb-lg-0' src={comic.thumbnail.path + ".jpg"} alt="comicCover" />
                 </div>
-                <br/>
-                <div className='comicCharacters'><strong>Characters: {this.displayCharacters()}</strong></div>
-                
-            </div> 
-        </div>
-        
+                <div className="col-lg-5 border border-secondary">
+                    <h1>{comic.title}</h1>
+                    <span className="comicDescription"><strong>Description: </strong> {comic.description}  </span>
+                    <br />
+                    <span><strong>Release Date: </strong> {this.displayDate(comic.dates[0].date)} </span>
+                    <br />
+                    <span><strong>Characters{this.displayCharacters()}</strong></span>
+                </div>
+            </div>
         )
     }
 }
