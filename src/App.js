86b748d4e92084b4959bf7249bce4029ac5f2efc
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-07 13:31:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,27 @@ import CharacterShow from './components/characters/CharacterShow'
 import ComicShow from './components/comics/ComicShow'
 import ComicContainer from './containers/ComicContainer'
 
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
 
   render() {
     return (
-        <Router>
-          <div className="App">
-            <Route exact path='/comics/:id' component={ComicShow} />
-            <Route exact path='/characters/:id' component={CharacterShow} />
-            <Route exact path='/search' component={SearchContainer} />
-            <Route exact path='/' component={SearchContainer} />
-            <Route exact path='/characters' component={CharacterContainer}/>
-            <Route exact path='/comics' component={ComicContainer}/>
-          </div>
-          <a href="/search">Search again</a>
-        </Router>
-      );
+      <Router>
+        <div className="App">
+          <Route exact path='/comics/:id' component={ComicShow} />
+          <Route exact path='/characters/:id' component={CharacterShow} />
+          <Route exact path='/search' component={SearchContainer} />
+          <Route exact path='/' component={SearchContainer} />
+          <Route exact path='/characters' component={CharacterContainer} />
+          <Route exact path='/comics' component={ComicContainer} />
+        </div>
+      </Router>
+    );
   }
 }
 
 
 
-  
+
 export default App;
