896e40ae1c6abb0122f55cd898494c690618c54b
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-07 13:31:49 -0500

    add class name to div

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -5,12 +5,12 @@ class Characters extends Component {
 
     render(){
         return (
-            <>
+            <div className="row align-items-center my-5 border border-secondary">
             <h1>Characters</h1>
                 <ol>
                     {this.props.characters ? this.props.characters.map(character => <Character key={character.id} character={character} />) : "No Characters"}
                 </ol>
-            </>
+            </div>
         )
     }
 }
