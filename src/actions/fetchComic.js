cb880e8432065804b3a47732f3a4859de3c9c7ae
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-08 17:13:54 -0500

    update formatting

--- src/actions/fetchComic.js
+++ src/actions/fetchComic.js
@@ -1,13 +1,13 @@
-export function fetchComic(string){
+export function fetchComic(string) {
     return (dispatch) => {
-        dispatch({type: 'START_ADDING_COMIC_REQUEST'});
+        dispatch({ type: 'START_ADDING_COMIC_REQUEST' });
         let md5 = require('md5')
         let date = new Date()
         let timeStamp = date.getTime()
         let hash = md5(`${timeStamp} + ${process.env.REACT_APP_API_PRIVATE_KEY} + ${process.env.REACT_APP_API_PUBLIC_KEY}`)
 
         fetch(`https://gateway.marvel.com:443/v1/public/comics/${string}?apikey=${process.env.REACT_APP_API_PUBLIC_KEY}&hash=${hash}`)
-        .then(resp => resp.json())
-        .then(comic => dispatch({type: 'ADD_COMIC', comic}))
+            .then(resp => resp.json())
+            .then(comic => dispatch({ type: 'ADD_COMIC', comic }))
     }
 }
--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Character extends Component {
 
     render() {
-       return (
-            <> 
+        return (
+            <>
                 <li><a href={`/characters/${this.props.character.id}`}>{this.props.character.name}</a></li>
             </>
         )
