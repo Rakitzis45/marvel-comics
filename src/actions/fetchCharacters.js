ab763453adf94e49739544a94310480c1e5ea22d
Rakitzis45 <67162811+Rakitzis45@users.noreply.github.com>
2021-03-12 15:38:08 -0500

    Update fetchCharacters.js

--- src/actions/fetchCharacters.js
+++ src/actions/fetchCharacters.js
@@ -8,7 +8,7 @@ export function fetchCharacters(string) {
 
         console.log(hash)
         fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${string}&apikey=${process.env.REACT_APP_API_PUBLIC_KEY}&hash=${hash}`)
-            // .then(resp => resp.json())
+            .then(resp => resp.json())
             .then(resp => console.log(resp))
             // .then(characters => dispatch({ type: 'ADD_SEARCH_CHARACTERS', characters }))
     }
